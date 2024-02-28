7a09e2440709330e97c9e20adc9d0f77f0363c2b
DuvanCamiloArenasRodriguez <dccar21@gmail.com>
2024-02-28 06:26:23 -0500

    feat: :tada: Creacion del proyecto
    Se crea el proyecto, asi como su estructura principal

---
+++ js/main.js
@@ -0,0 +1,366 @@
+const marvel = [{
+    "id": "5d86371f1efebc31def272e2",
+    "about": "Tony Stark, un genio multimillonario, se convierte en Iron Man gracias a un traje de alta tecnología que él mismo construye para luchar contra el mal.",
+    "picture": "https://cdn.marvel.com/content/1x/002irm_ons_crd_03.jpg",
+    "squarePic": "https://dam.smashmexico.com.mx/wp-content/uploads/2018/06/27181227/ironman_portada2.jpg",
+    "name": "Iron Man"
+},
+{
+    "id": "5d86371f2343e37870b91ef1",
+    "about": "El Dr. Bruce Banner se transforma en Hulk, una criatura verde y poderosa, cuando está enojado. Tiene una fuerza sobrehumana.",
+    "picture": "https://cdn.marvel.com/content/1x/006hbb_ons_crd_03.jpg",
+"squarePic": "https://bolavip.com/__export/1557252167649/sites/bolavip/img/2019/05/07/hulk_crop1557252167265.jpg_1693159006.jpg",
+    "name": "Hulk"
+},
+{
+    "id": "5d86371f25a058e5b1c8a65e",
+    "about": "Laboris est duis eiusmod adipisicing cillum ut sit ea Lorem non laboris quis Lorem. Est culpa esse aliqua non labore dolor esse labore nulla mollit. Nostrud amet est quis adipisicing dolor. Labore veniam elit veniam non ad ex consequat excepteur eiusmod. Minim cupidatat velit Lorem culpa quis consequat incididunt qui amet incididunt pariatur ex aliquip aliqua. Magna ex elit in aliquip minim eu ut ut fugiat ullamco deserunt adipisicing cillum.",
+    "picture": "https://cdn.marvel.com/content/1x/003cap_ons_crd_03.jpg",
+    "name": "Captain America"
+},
+{
+    "id": "5d86371f9f80b591f499df32",
+    "about": "También conocida como Carol Danvers, es una piloto de la Fuerza Aérea de Estados Unidos que obtiene superpoderes después de un accidente con tecnología alienígena.",
+    "picture": "https://cdn.marvel.com/content/1x/008cmv_ons_crd_05.jpg",
+"squarePic": "https://img.chilango.com/2018/05/quien-es-capitan-marvel.jpg",
+    "name": "Captain Marvel"
+},
+{
+    "id": "5d86371f233c9f2425f16916",
+    "about": "Natasha Romanoff es una espía y experta en combate cuerpo a cuerpo, además de ser una maestra del espionaje y la infiltración.",
+    "picture": "https://cdn.marvel.com/content/1x/011blw_ons_crd_04.jpg",
+    "squarePic": "https://wipy.tv/wp-content/uploads/2019/07/filtran-el-tr%C3%A1iler-de-%E2%80%98Black-Widow%E2%80%99.jpg",
+    "name": "Black Widow"
+},
+{
+    "id": "5d86371f97c29d020f1e1f6d",
+    "about": "Peter Quill es un aventurero espacial y líder de los Guardianes de la Galaxia. Es conocido por su estilo irreverente y su habilidad para resolver problemas.",
+    "picture": "https://cdn.marvel.com/content/1x/021slq_ons_crd_03.jpg",
+    "squarePic": "https://www.tooys.mx/pub/media/catalog/product/cache/89d4a95274a31fe8bdfcc437494b2c9e/f/a/falcon_marvel_gallery_5c4dc5df6c14d_-_copia.jpg",
+    "name": "Star-Lord"
+},
+{
+    "id": "5d86371fd55e2e2a30fe1ccb",
+    "about": "T'Challa es el rey de Wakanda y posee habilidades sobrehumanas gracias a la planta en forma de corazón. Se convierte en el protector de su nación como Black Panther.",
+    "picture": "https://cdn.marvel.com/content/1x/007blp_ons_crd_02.jpg",
+"squarePic": "http://www.mundocinematografico.net/wp-content/uploads/2018/02/black-panther-marvel-cosas-felices.jpg",
+    "name": "Black Panther"
+},
+{
+    "id": "5d86371fd55e2e2a30fe1ccb1",
+    "about": "El Dr. Stephen Strange era un cirujano famoso hasta que un accidente lo deja con daño en las manos. Busca la ayuda del Anciano y se convierte en el Hechicero Supremo, protector del universo.",
+    "picture": "https://cdn.marvel.com/content/1x/009drs_ons_crd_03.jpg",
+"squarePic": "https://img-cdn.hipertextual.com/files/2019/09/hipertextual-filtracion-revela-que-doctor-strange-2-resucitara-dos-personajes-muertos-marvel-2019435911.jpg?strip=all&lossy=1&quality=57&resize=740%2C490&ssl=1",
+    "name": "Doctor Strange"
+}, {
+    "id": "5d86371fd55e2e2a30fe1ccb2",
+    "about": "Peter Parker es un estudiante de secundaria que adquiere habilidades arácnidas después de ser picado por una araña radioactiva. Lucha contra el crimen como Spider-Man.",
+    "picture": "https://cdn.marvel.com/content/1x/005smp_ons_crd_02.jpg",
+"squarePic": "https://e00-marca.uecdn.es/assets/multimedia/imagenes/2019/09/28/15696592030335.jpg",
+    "name": "Spider Man"
+},
+{
+    "id": "5d86371fd55e2e2a30fe1cc3",
+    "about": "Scott Lang es un ladrón que se convierte en el superhéroe Ant-Man después de robar un traje de alta tecnología que le permite encogerse de tamaño y aumentar su fuerza.",
+    "picture": "https://cdn.marvel.com/content/1x/010ant_ons_crd_05.jpg",
+"squarePic": "https://img.chilango.com/2018/05/quien-es-capitan-marvel.jpg",
+    "name": "Ant Man"
+},
+{
+    "id": "5d86371fd55e2e2a30fe1cc4",
+    "about": "Hope van Dyne es una experta en artes marciales y una piloto habilidosa. Se convierte en la superheroína Wasp gracias a un traje similar al de Ant-Man.",
+    "picture": "https://cdn.marvel.com/content/1x/321whv_ons_crd_02.jpg",
+"squarePic": "https://img.chilango.com/2018/05/quien-es-capitan-marvel.jpg",
+    "name": "The Wasp"
+
+},
+{
+    "id": "5d86371fd55e2e2a30fe1cc8",
+    "about": "Es el dios de las travesuras y el hermano adoptivo de Thor. A menudo es un villano, pero ocasionalmente se une a los héroes para luchar contra amenazas mayores.",
+    "picture": "https://cdn.marvel.com/content/1x/017lok_ons_crd_03.jpg",
+    "squarePic": "https://img.chilango.com/2018/05/quien-es-capitan-marvel.jpg",
+    "name": "Loki"
+},
+{
+    "id": "5d86371fd55e2e2a30fe1cc9",
+    "about": "Es el príncipe de Asgard y el dios del trueno. Posee una fuerza y resistencia sobrehumanas, así como la habilidad de controlar el martillo Mjolnir.",
+    "picture": "https://cdn.marvel.com/content/1x/004tho_ons_crd_04.jpg",
+    "squarePic": "https://img.chilango.com/2018/05/quien-es-capitan-marvel.jpg",
+    "name": "Thor"
+    
+},
+{
+    "id": "5d86371fd55e2e2a30fe1cc9",
+    "about": "Matt Murdock es un abogado ciego que, gracias a sus otros sentidos mejorados, lucha contra el crimen en la ciudad de Nueva York como el superhéroe Daredevil.",
+    "picture": "https://cdn.marvel.com/content/1x/026ddm_ons_crd_02_1.jpg",
+    "squarePic": "https://img.chilango.com/2018/05/quien-es-capitan-marvel.jpg",
+    "name": "Daredevil"
+    
+},
+{
+    "id": "5d86371fd55e2e2a30fe1cc9",
+    "about": "Wade Wilson es un mercenario con un sentido del humor único que adquiere habilidades de curación regenerativa después de un experimento. Se dedica a luchar contra el crimen, a menudo rompiendo la cuarta pared.",
+    "picture": "https://cdn.marvel.com/content/1x/036dpl_com_crd_01.jpg",
+    "squarePic": "https://img.chilango.com/2018/05/quien-es-capitan-marvel.jpg",
+    "name": "Deadpool"
+    
+},
+{
+    "id": "5d86371fd55e2e2a30fe1cc9",
+    "about": "También conocido como Logan, es un mutante con garras retráctiles y un factor de curación acelerado. Es conocido por su actitud ruda y su pasado misterioso.",
+    "picture": "https://cdn.marvel.com/content/1x/034wlv_com_crd_01.jpg",
+    "squarePic": "https://img.chilango.com/2018/05/quien-es-capitan-marvel.jpg",
+    "name": "Wolverine"
+    
+}
+
+
+]
+
+
+const DC = [{
+    "id": "5d86371f1efebc31def272e1",
+    "about": "Bruce Wayne es un multimillonario que se convierte en el justiciero encapuchado de Gotham City después de presenciar el asesinato de sus padres. Utiliza su inteligencia, habilidades de detective y tecnología avanzada para luchar contra el crimen como Batman.",
+    "picture": "https://i.pinimg.com/564x/42/e6/b4/42e6b484930e3bbc330610da34d4648b.jpg",
+    "squarePic": "https://dam.smashmexico.com.mx/wp-content/uploads/2018/06/27181227/ironman_portada2.jpg",
+    "name": "Batman"
+},
+{
+    "id": "5d86371f2343e37870b91ef2",
+    "about": "Clark Kent, también conocido como Kal-El, es un alienígena que fue enviado a la Tierra desde el planeta Krypton. Posee una fuerza y ​​velocidad sobrehumanas, vuelo, visión de rayos X y otros poderes que utiliza para proteger a la humanidad como Superman.",
+    "picture": "https://i.pinimg.com/564x/82/34/fc/8234fc5238f4c84fc117ef9903f79b36.jpg",
+"squarePic": "https://bolavip.com/__export/1557252167649/sites/bolavip/img/2019/05/07/hulk_crop1557252167265.jpg_1693159006.jpg",
+    "name": "Superman"
+},
+{
+    "id": "5d86371f25a058e5b1c8a65e",
+    "about": "También conocida como Diana Prince, es una guerrera amazona de la isla de Themyscira. Dotada de fuerza sobrehumana, velocidad y agilidad, así como habilidades de vuelo y habilidades en combate cuerpo a cuerpo, Wonder Woman lucha por la justicia, la paz y la igualdad en el mundo, utilizando su Lazo de la Verdad y otros artefactos místicos para ayudar en su misión.",
+    "picture": "https://i.pinimg.com/564x/52/b6/15/52b615b75c6d7d067e5a82eb6e622f22.jpg",
+    "name": "Wonder Woman"
+},
+{
+    "id": "5d86371f9f80b591f499df32",
+    "about": "Arthur Curry es el rey de la Atlántida y tiene la capacidad de comunicarse con criaturas marinas, así como fuerza sobrehumana y la capacidad de respirar bajo el agua.",
+    "picture": "https://i.pinimg.com/564x/a1/c7/16/a1c716e4dadcf3219e7394d155d2fb0d.jpg",
+"squarePic": "https://img.chilango.com/2018/05/quien-es-capitan-marvel.jpg",
+    "name": "Aquaman"
+},
+{
+    "id": "5d86371f233c9f2425f16916",
+    "about": "Dick Grayson, el primer Robin, se convierte en Nightwing después de dejar de ser el compañero de Batman. Es un experto acróbata y luchador, y lidera su propio equipo de superhéroes en Blüdhaven.",
+    "picture": "https://i.pinimg.com/564x/ec/28/e2/ec28e238d9f8cfaca7bd417082f3bc99.jpg",
+    "squarePic": "https://wipy.tv/wp-content/uploads/2019/07/filtran-el-tr%C3%A1iler-de-%E2%80%98Black-Widow%E2%80%99.jpg",
+    "name": "Nightwing"
+},
+{
+    "id": "5d86371f97c29d020f1e1f6d",
+    "about": "Barry Allen es un científico forense que obtiene super velocidad después de ser alcanzado por un rayo y bañado en productos químicos. Se convierte en el héroe conocido como The Flash.",
+    "picture": "https://i.pinimg.com/564x/b1/97/f1/b197f1c37f507c884c5b185c8d0b4028.jpg",
+    "squarePic": "https://www.tooys.mx/pub/media/catalog/product/cache/89d4a95274a31fe8bdfcc437494b2c9e/f/a/falcon_marvel_gallery_5c4dc5df6c14d_-_copia.jpg",
+    "name": "The Flash"
+},
+{
+    "id": "5d86371fd55e2e2a30fe1ccb",
+    "about": "J'onn J'onzz es el último superviviente de Marte y tiene una variedad de habilidades telepáticas, de cambio de forma y fuerza sobrehumana, que utiliza para luchar por la justicia en la Tierra.",
+    "picture": "https://i.pinimg.com/564x/74/46/f2/7446f207098c97548f766dd76c28494e.jpg",
+"squarePic": "http://www.mundocinematografico.net/wp-content/uploads/2018/02/black-panther-marvel-cosas-felices.jpg",
+    "name": "Martian Manhunter"
+},
+{
+    "id": "5d86371fd55e2e2a30fe1ccb1",
+    "about": "Shiera Hall, también conocida como Kendra Saunders, es una guerrera alada con habilidades de vuelo, fuerza mejorada y una conexión mística con la reencarnación de su alma.",
+    "picture": "https://i.pinimg.com/564x/6d/b9/49/6db949e846ade0435501aaa1b4b0a234.jpg",
+"squarePic": "https://img-cdn.hipertextual.com/files/2019/09/hipertextual-filtracion-revela-que-doctor-strange-2-resucitara-dos-personajes-muertos-marvel-2019435911.jpg?strip=all&lossy=1&quality=57&resize=740%2C490&ssl=1",
+    "name": "Hawkgirl"
+}, 
+{
+    "id": "5d86371fd55e2e2a30fe1ccb2",
+    "about": "Varios personajes han asumido el papel de Green Lantern, cada uno perteneciente a la Fuerza de Green Lantern y portando un anillo de poder que les otorga la capacidad de crear construcciones de energía pura.",
+    "picture": "https://i.pinimg.com/564x/f8/7e/05/f87e059995abb857336357ea5d5ab13e.jpg",
+"squarePic": "https://e00-marca.uecdn.es/assets/multimedia/imagenes/2019/09/28/15696592030335.jpg",
+    "name": "Green Lantern"
+},
+{
+    "id": "5d86371fd55e2e2a30fe1cc3",
+    "about": "Billy Batson es un niño que puede transformarse en el superhéroe Shazam diciendo la palabra mágica "+ '¡Shazam!' + ". Obtiene una variedad de poderes, incluida la fuerza de Hércules, la velocidad de Mercurio y la resistencia de Atlas.",
+    "picture": "https://i.pinimg.com/564x/89/53/80/895380c3c9b2d77930a5ca72f0b6f35d.jpg",
+"squarePic": "https://img.chilango.com/2018/05/quien-es-capitan-marvel.jpg",
+    "name": "Shazam"
+},
+{
+    "id": "5d86371fd55e2e2a30fe1cc4",
+    "about": "Ray Palmer es un científico que desarrolla un traje que le permite encogerse hasta el tamaño subatómico, así como controlar la tecnología de reducción.",
+    "picture": "https://i.pinimg.com/564x/b1/7c/bd/b17cbd1bd11addf322509d18a6c39ed1.jpg",
+"squarePic": "https://img.chilango.com/2018/05/quien-es-capitan-marvel.jpg",
+    "name": "Atom"
+
+},
+{
+    "id": "5d86371fd55e2e2a30fe1cc8",
+    "about": "Jon Osterman adquiere poderes cuánticos después de un accidente en un reactor nuclear, convirtiéndose en el ser omnipotente conocido como Dr. Manhattan.",
+    "picture": "https://i.pinimg.com/564x/43/1d/b3/431db3a5804a4e0a43c63f3443671cbc.jpg",
+    "squarePic": "https://img.chilango.com/2018/05/quien-es-capitan-marvel.jpg",
+    "name": "Dr Manhattan"
+},
+{
+    "id": "5d86371fd55e2e2a30fe1cc9",
+    "about": "Es un mago y exorcista inglés conocido por su cinismo y habilidades en la magia negra. Constantine lucha contra amenazas sobrenaturales y demoníacas.",
+    "picture": "https://i.pinimg.com/564x/fd/36/f6/fd36f62430f3f6b583cf6f0faacbac39.jpg",
+    "squarePic": "https://img.chilango.com/2018/05/quien-es-capitan-marvel.jpg",
+    "name": "John Constantine"
+    
+},
+{
+    "id": "5d86371fd55e2e2a30fe1cc9",
+    "about": "Varios personajes han asumido el papel de Doctor Fate, cada uno portando el casco del destino, que les otorga una amplia gama de habilidades místicas.",
+    "picture": "https://i.pinimg.com/564x/5f/8e/25/5f8e25fa32426a894a3d6ad789466354.jpg",
+    "squarePic": "https://img.chilango.com/2018/05/quien-es-capitan-marvel.jpg",
+    "name": "Doctor Fate"
+    
+},
+{
+    "id": "5d86371fd55e2e2a30fe1cc9",
+    "about": "Zatanna Zatara es una maga poderosa que realiza hechizos hablando al revés. Es una experta en magia de escenario y una poderosa aliada en la lucha contra el mal.",
+    "picture": "https://i.pinimg.com/564x/d9/c6/cb/d9c6cb7e30251b8071cbd587ec34ee9e.jpg",
+    "squarePic": "https://img.chilango.com/2018/05/quien-es-capitan-marvel.jpg",
+    "name": "Zatanna"
+    
+},
+{
+    "id": "5d86371fd55e2e2a30fe1cc9",
+    "about": "Victor Stone es un joven atleta que, después de un accidente, es reconstruido con partes cibernéticas, otorgándole fuerza sobrehumana, tecnopatía y la capacidad de conectarse con sistemas informáticos.",
+    "picture": "https://i.pinimg.com/564x/d8/0c/20/d80c20ac77e03a15b9929f39de0c9e60.jpg",
+    "squarePic": "https://img.chilango.com/2018/05/quien-es-capitan-marvel.jpg",
+    "name": "Cyborg"
+    
+}
+]
+
+
+
+
+const cuerpo = document.querySelector('.cuerpo')
+marvel.forEach((item) => {
+    // const marvel = document.createElement("DIV")
+    const caracter = document.createElement("DIV")
+    const foto = document.createElement("IMG")
+    const contenido = document.createElement("DIV")
+    const titulo = document.createElement("H3")
+    const boton = document.createElement("BUTTON")
+    boton.id = item.name
+
+    
+    // Asignar clases
+    // marvel.classList.add('marvel')
+    caracter.classList.add('caracter')
+    foto.classList.add('imagen')
+    contenido.classList.add('contenido')
+    titulo.classList.add('nombrePersonaje')
+    boton.classList.add('ver')
+    boton.innerHTML = "Ver"
+    foto.src = item.picture
+    titulo.textContent = item.name
+    
+    // Anidar elementos
+    // marvel.appendChild(cuerpo)
+    caracter.appendChild(foto)
+    caracter.appendChild(contenido)
+    contenido.appendChild(titulo)
+    contenido.appendChild(boton)
+    cuerpo.appendChild(caracter)
+
+    const modal = document.querySelector(".ventana")
+    // const botonModal = document.querySelector(".ver")
+
+    boton.addEventListener('click', function() {
+    modal.style.display = "block"
+    console.log("sdjsd.kg")
+})
+})
+
+let cuerpoDC = document.querySelector('.DC .cuerpo')
+// let cuerp = cuerpoDC.querySelector('.cuerpo')
+console.log(cuerpoDC);
+DC.forEach((item) => {
+    // const dc = document.createElement("DIV")
+    const caracter = document.createElement("DIV")
+    const foto = document.createElement("IMG")
+    const contenido = document.createElement("DIV")
+    const titulo = document.createElement("H3")
+    const boton = document.createElement("BUTTON")
+    
+    // Asignar clases
+    // dc.classList.add('DC')
+    caracter.classList.add('caracter')
+    foto.classList.add('imagen')
+    contenido.classList.add('contenido')
+    titulo.classList.add('nombrePersonaje')
+    boton.classList.add('ver')
+    boton.innerHTML = "Ver"
+    foto.src = item.picture
+    titulo.textContent = item.name
+    boton.id = item.name
+    
+    // Anidar elementos
+    // dc.appendChild(caracter)
+    caracter.appendChild(foto)
+    caracter.appendChild(contenido)
+    contenido.appendChild(titulo)
+    contenido.appendChild(boton)
+    cuerpoDC.appendChild(caracter)
+    
+})
+
+// Creacion de modal
+const modal = document.querySelector(".ventana")
+const boton = document.querySelector(".ver")
+
+boton.addEventListener('click', () => {
+    modal.style.display = "block"
+    console.log("sdjsd.kg")
+})
+
+const cerrar = document.querySelector(".cerrar")
+cerrar.addEventListener('click', () => {
+    modal.style.display = "none"
+})
+
+// Detectar click para el dialog dinámico Marvel
+const seccionMarvel = document.querySelector(".marvel")
+seccionMarvel.addEventListener('click', (evento) => {
+    
+    if (evento.target.classList.contains("ver")) {
+        console.log(evento.target.id);
+        buscarPersonaje(evento.target.id)
+    }
+})
+
+function buscarPersonaje(nombre) {
+    const guardarPersonaje = marvel.find((item)=> item.name === nombre)
+    console.log(guardarPersonaje)
+    mostrarModal(guardarPersonaje)
+}
+
+function mostrarModal(heroe) {
+    const titulo = document.querySelector('.tituloModal')
+    const descripcion = document.querySelector('.descripcion')
+    const foto = document.querySelector('.imgModal')
+   
+    titulo.textContent = heroe.name
+    descripcion.textContent = heroe.about
+    foto.src = heroe.picture
+}
+
+// Detectar click para el dialog dinámico DC
+const seccionDC = document.querySelector(".DC")
+seccionDC.addEventListener('click', (evento) => {
+    if (evento.target.classList.contains("ver")) {
+        buscarPersonajeDC(evento.target.id)
+    }
+})
+
+function buscarPersonajeDC(nombre) {
+    const guardarPersonaje = DC.find((item)=> item.name === nombre)
+    console.log(guardarPersonaje)
+    mostrarModal(guardarPersonaje)
+}
